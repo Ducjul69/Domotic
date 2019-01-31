a5c429d79697ab26159c4828411d232185e35813
Ducjul <46600455+Ducjul@users.noreply.github.com>
2019-01-31 21:55:18 +0100

    Add files via upload

--- Programme_Domotic/programme_communication_temperature_ext.py
+++ Programme_Domotic/programme_communication_temperature_ext.py
@@ -5,14 +5,18 @@ import time
 import threading
 from tkinter import messagebox
 
+import asyncio
+import websockets
 
 from programme_outil_db import*
 
 ############################################
 def communication_esp_temperature_ext():
+    print ("tesrer")
     #connection a l esp8266 
     ws = websocket.WebSocket()
     ws.connect("ws://192.168.1.102/")
+    print("truc")
     ws.send("0")
     humidite = ws.recv()
     humidite=float(humidite)
@@ -20,7 +24,7 @@ def communication_esp_temperature_ext():
     variable_input = "hygrometrie_exterieur"
     variable_etat = int(humidite)
     update_db(variable_input, variable_etat)
-    time.sleep(1)
+    time.sleep(0.5)
         
     ws.send("1")
     temperature = ws.recv()
@@ -29,43 +33,42 @@ def communication_esp_temperature_ext():
     variable_input = "temperature_exterieur"
     variable_etat = int(temperature)
     update_db(variable_input, variable_etat)
-    time.sleep(1)
+    time.sleep(0.5)
         
     ws.send("2")
     resenti = ws.recv()
     resenti=float(resenti)
-    time.sleep(1)
+    time.sleep(0.5)
     ws.close()
         
 
 ############################################
 def changement_etat():
-    i=0
+    """   i=0
     while 1:
         try:
+            print("trucd")
             communication_esp_temperature_ext()
             #ecriture bd pas de pb sur la com
             variable_input = "temperature_ext_error"
             variable_etat = 0
             update_db(variable_input, variable_etat)
-            print("truc")
-            time.sleep(60)
+
         except:
-            #chargement dans la base
-            variable_input = "hygrometrie_exterieur"
-            variable_etat = 0
-            update_db(variable_input, variable_etat)
-            #chargement dans la base
-            variable_input = "temperature_exterieur"
-            variable_etat = 0
-            update_db(variable_input, variable_etat)
-            #ecriture bd pb sur la com
-            variable_input = "temperature_ext_error"
-            variable_etat = 1
-            update_db(variable_input, variable_etat)
-            time.sleep(60)
+            a=1"""
+
+
+    async def echo(websocket, path):
+        async for message in websocket:
+            await websocket.send(message)
+
+    asyncio.get_event_loop().run_until_complete(websockets.serve(echo, 'localhost', 8765))
+    asyncio.get_event_loop().run_forever()
+    print(messsage)
         
-############################################
+
+
+
 def com_esp_temperature_ext():
     global t1
     #demarrage du thread
